a0172658e2ca96a862ce0a9166e8f96e6c313bca
wangh <419264755@qq.com>
2020-03-08 16:11:12 +0800

    4-2

---
+++ latte01/latte-service/src/main/java/com/wh/service/center/CenterUserService.java
@@ -0,0 +1,29 @@
+package com.wh.service.center;
+
+import com.wh.pojo.Users;
+import com.wh.pojo.bo.center.CenterUserBO;
+
+/**
+ * @author: wh
+ * @create: 2020/3/8 11:48
+ */
+public interface CenterUserService {
+
+    /**
+     * 根据用户id查询用户信息
+     *
+     * @param userId
+     * @return
+     */
+    Users queryUserInfo(String userId);
+
+    /**
+     * 修改用户信息
+     *
+     * @param centerUserBO
+     * @param userId
+     * @return
+     */
+    Users updateUserInfo(CenterUserBO centerUserBO, String userId);
+
+}
